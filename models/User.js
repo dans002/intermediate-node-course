40e8980b63048f1ae7737613c65b64d572b19183
dans002 <60510988+dans002@users.noreply.github.com>
2022-04-21 21:34:45 +0900

    add a create and get method

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-	name: {type: string, required: true},
-	email: {type, string, required: true, unique: true},
-	password: {type: string, required: true}
+	name: {type: String, required: true},
+	email: {type: String, required: true, unique: true},
+	password: {type: String, required: true}
 });
 
-moudel.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('User', UserSchema)
